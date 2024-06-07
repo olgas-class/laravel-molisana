3a8cb6765a66863c89e4adda7d4b28a8f2c9a049
olga_demina <olga.demina1410@gmail.com>
2024-06-07 12:28:05 +0200

    feat: header partial

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+  <div class="text-center">
+    <img src="{{ Vite::asset('resources/img/logo.png') }}" alt="Logo Molisana">
+  </div>
+  <ul class="d-flex justify-content-center gap-3">
+    <li><a href="{{ route('home') }}">Home</a></li>
+    <li><a href="{{ route('products') }}">Prodotti</a></li>
+    <li><a href="{{ route('news') }}">News</a></li>
+  </ul>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name("home");
 
 Route::get('/news', function () {
     return view('news');
-});
+})->name("news");
 
 Route::get('/products', function () {
     return view('products');
-});
+})->name("products");
