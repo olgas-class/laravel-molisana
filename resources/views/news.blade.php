454a12333921a84e57da6bd80ed49affd8477ac7
olga_demina <olga.demina1410@gmail.com>
2024-06-07 12:21:29 +0200

    feat: basic pages created

---
+++ resources/views/news.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+  <h1>News page</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/news', function () {
+    return view('news');
+});
+
+Route::get('/products', function () {
+    return view('products');
+});
